abeea28435f4bf226aee38916ab64f6a80cfa323
paopao <fantao@shantaijk.cn>
2024-03-23 18:55:44 +0800

    add

--- micro-app1/src/index.js
+++ micro-app1/src/index.js
@@ -18,15 +18,17 @@
 
 import { singleSpaPacker } from "./utils";
 import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// if (process.env.NODE_ENV === "development") {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>,
-//     document.getElementById("root")
-//   );
-// }
-
+if (!process.env.mode) {
+  require("./index.css");
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 const { bootstrap, mount, unmount } = singleSpaPacker(App);
 export { bootstrap, mount, unmount };
--- micro-app2/src/index.js
+++ micro-app2/src/index.js
@@ -1,6 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
@@ -18,15 +17,18 @@
 
 import { singleSpaPacker } from "./utils";
 import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// if (process.env.NODE_ENV === "development") {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>,
-//     document.getElementById("root")
-//   );
-// }
+if (!process.env.mode) {
+  require("./index.css");
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 const { bootstrap, mount, unmount } = singleSpaPacker(App);
 export { bootstrap, mount, unmount };
